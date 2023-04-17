6129b2404062c49a9e501ae1f00ae261253331da
Brian Bissette <bbissette3@yahoo.com>
2023-04-17 16:38:39 -0500

    created models, created tables, created debug.py, added relationships with foreign keys, added data to tables with debug.py

---
+++ lib/db/debug.py
@@ -0,0 +1,70 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import (Base, Armor, Weapon, Specialty, Character, User)
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///character_builder.db")
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Armor).delete()
+    session.commit()
+
+    aluminum = Armor(name="Aluminum", speed=9, defense=3)
+    diamond = Armor(name="Diamond", speed=5, defense=10)
+    wood = Armor(name="Wood", speed=7, defense=7)
+    gold = Armor(name="Gold", speed=6, defense=9)
+
+    session.add(aluminum)
+    session.add(diamond)
+    session.add(wood)
+    session.add(gold)
+    session.commit()
+
+    session.query(Weapon).delete()
+    session.commit()
+
+    chainsaw = Weapon(name="Chainsaw", speed=2, damage=10)
+    chainballwhip = Weapon(name="Chain Ball Whip", speed=7, damage=10)
+    poisongas = Weapon(name="Poison Gas", speed=4, damage=9)
+    tasergun = Weapon(name="Taser Gun", speed=10, damage=5)
+
+    session.add(chainsaw)
+    session.add(chainballwhip)
+    session.add(poisongas)
+    session.add(tasergun)
+    session.commit()
+
+    session.query(Specialty).delete()
+    session.commit()
+
+    liar = Specialty(name="Liar", defense=10, damage=3)
+    builder = Specialty(name="Builder", defense=8, damage=0)
+    swimmer = Specialty(name="Swimmer", defense=0, damage=3)
+    gentleperson = Specialty(name="Gentle Person", defense=5, damage=5)
+    
+    session.add(liar)
+    session.add(builder)
+    session.add(swimmer)
+    session.add(gentleperson)
+    session.commit()
+
+    session.query(User).delete()
+    session.commit()
+
+    trollbot = User(username="Troll Bot")
+
+    session.add(trollbot)
+    session.commit()
+
+    session.query(Character).delete()
+    session.commit()
+
+    kirby = Character(name="Kirby", defense=10, damage=9, speed=5, health=10, armor_id=gold.id, weapon_id=tasergun.id, specialty_id=liar.id, user_id=trollbot.id)
+
+    session.add(kirby)
+    session.commit()
+
+import ipdb; ipdb.set_trace()
---
+++ lib/db/models.py
@@ -0,0 +1,90 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+class Armor(Base):
+    __tablename__ = "armors"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    speed = Column(Integer())
+    defense = Column(Integer())
+    characters = relationship("Character", backref=backref("armor"))
+
+    def __repr__(self):
+        return f"Id: {self.id}, " \
+            + f"Name: {self.name}, " \
+            + f"Speed: {self.speed}, " \
+            + f"Defense: {self.defense}"
+    
+class Weapon(Base):
+    __tablename__ = "weapons"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    speed = Column(Integer())
+    damage = Column(Integer())
+    characters = relationship("Character", backref=backref("weapon"))
+
+    def __repr__(self):
+        return f"Id: {self.id}, " \
+            + f"Name: {self.name}, " \
+            + f"Speed: {self.speed}, " \
+            + f"Damage: {self.damage}"
+    
+class Specialty(Base):
+    __tablename__ = "specialties"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    defense = Column(Integer())
+    damage = Column(Integer())
+    characters = relationship("Character", backref=backref("specialty"))
+
+    def __repr__(self):
+        return f"Id: {self.id}, " \
+            + f"Name: {self.name}, " \
+            + f"Defense: {self.defense}, " \
+            + f"Damage: {self.damage}"
+    
+class Character(Base):
+    __tablename__ = "characters"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    defense = Column(Integer())
+    damage = Column(Integer())
+    speed = Column(Integer())
+    health = Column(Integer())
+    armor_id = Column(Integer(), ForeignKey("armors.id"))
+    weapon_id = Column(Integer(), ForeignKey("weapons.id"))
+    specialty_id = Column(Integer(), ForeignKey("specialties.id"))
+    user_id = Column(Integer(), ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f"Id: {self.id}, " \
+            + f"Name: {self.name}, " \
+            + f"Defense: {self.defense}, " \
+            + f"Damage: {self.damage}", \
+            + f"Health: {self.health}", \
+            + f"Armor Id: {self.armor_id}, " \
+            + f"Weapon Id: {self.weapon_id}, " \
+            + f"Speialty Id: {self.specialty_id}, " \
+            + f"User Id: {self.user_id}"
+    
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key=True)
+    username = Column(String())
+    characters = relationship("Character", backref=backref("user"))
+
+    def __repr__(self):
+        return f"Username: {self.username}"
