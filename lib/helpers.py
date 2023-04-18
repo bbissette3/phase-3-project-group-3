6afdf0d573de3fe84f4281ad6fc4b28dec571ca1
Brian Reardon <brian.d.reardon@gmail.com>
2023-04-18 11:01:07 -0400

    Set up skeleton of CLI and Helper functions

---
+++ lib/helpers.py
@@ -0,0 +1,62 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db.models import (Base, Armor, Weapon, Specialty, Character, User)
+
+
+
+
+#Login Section
+def start_menu():
+    pass
+
+def log_in():
+    pass
+
+def create_account():
+    pass
+
+
+
+# Main Section
+def main_menu():
+    pass
+
+def log_out():
+    pass
+
+def about():
+    pass
+
+
+# Character Section
+def character_menu():
+    pass
+
+def select_character():
+    pass
+
+def create_character():
+    pass
+
+def update_character():
+    pass
+
+def delete_character():
+    pass
+
+
+# Battle Section
+def battle_menu():
+    pass
+
+def playver_v_player():
+    pass
+
+def player_v_cpu():
+    pass
+
+
+
+
+#THE GAME
+# Implement this part last
